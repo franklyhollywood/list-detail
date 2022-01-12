66615806713353d47a7ed11f89f04938f97e0f4f
Frank Hollywood <github@srvsync.com>
2022-01-12 08:45:35 -0800

    fixed characters buttons

--- src/components/Characters/Characters.jsx
+++ src/components/Characters/Characters.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import styles from '../../App.css';
-import { Flex, Center, Grid, SimpleGrid, Box } from '@chakra-ui/react';
+import { Flex, Center, Grid, SimpleGrid, Box, Button } from '@chakra-ui/react';
 export default function Characters({ characters }) {
   return (
     <ul className={styles.ul}>
@@ -8,7 +8,7 @@ export default function Characters({ characters }) {
         <li className={styles.li} key={character._id}>
           <Link to={`/character/${character._id}`}>
             <img src={character.photoUrl} alt={character.name} width="200" />
-            <div>name: {character.name}</div>
+            <Button>name: {character.name}</Button>
           </Link>
         </li>
       ))}
